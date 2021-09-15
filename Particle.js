e3dae9a894fe64fbadab4bf75af102bde4e5158d
Shivangi212 <75981391+Shivangi212@users.noreply.github.com>
2021-09-15 16:38:21 +0530

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -20,7 +20,7 @@ class Particles {
         rotate(angle);
         imageMode(CENTER);
         noStroke();
-        fill("white");
+        fill(color(random(0,255),random(0,255),random(0,255)));
         ellipseMode(RADIUS);
         ellipse(0,0,this.r,this.r);
         pop();
